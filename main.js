92e5694fc3004b296290a7ad184d9bafc874e621
YounesM <younes@mgharfaoui.com>
2019-07-23 17:45:24 +0200

    Added POST/PUT/DELETE methods

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const {app, BrowserWindow, ipcMain} = require('electron');
+const {app, BrowserWindow, ipcMain, dialog} = require('electron');
 const https = require('https');
 const http = require('http');
 
 function createWindow () {
   let win = new BrowserWindow({
     width: 1000,
+    height:600,
     frame: false,
     backgroundColor: "#4a4a4a",
     webPreferences: {
@@ -22,15 +23,34 @@ app.on('ready', createWindow);
 ipcMain.on('close', () => {app.quit()});
 ipcMain.on('send', (events, args) => {
   let call = new RegExp('^https:\/\/').test(args.url) ? https : http;
-  call.get(args.url, {headers: args.headers}, res => {
-    console.log('pk')
+
+  //Check if method is correct
+  if(!['GET', 'POST', 'PUT', 'DELETE'].includes(args.method)) {
+    dialog.showErrorBox('Error','The method "'+ args.method + '" is not supported.');
+  }
+  let req = call.request(args.url, {method: args.method, headers: args.headers}, res => {
+    let data = "";
     res.setEncoding('utf8');
     res.on('data', (chunk) => {
       console.log(`BODY: ${chunk}`);
-      events.sender.send('response', chunk);
+      data += chunk;
+      events.sender.send('response', data);
+      events.sender.send('details',
+        {
+          headers: res.headers,
+          code: res.statusCode,
+          originUrl: args.url,
+          method: res.method
+        }
+      );
     });
     res.on('end', () => {
       console.log('No more data in response.');
     });
-  }).on('error', err => console.log(err.message));
+  });
+  if(['PUT', 'POST'].includes(args.method)) {
+    req.write(args.reqData);
+  }
+  req.on('error', err => console.log(err.message));
+  req.end();
 });
--- pages/main.js
+++ pages/main.js
@@ -1,19 +1,25 @@
 const {ipcRenderer} = require('electron');
 
-const sendBtn = document.querySelector('#send_rqst');
+const sendBtn = document.querySelector('#send-rqst');
+const addBody = document.querySelector('#add-body');
 const closeBtn = document.querySelector('.small-red');
 const minimizeBtn = document.querySelector('.small-yellow');
 const maximizeBtn = document.querySelector('.small-green');
 const responseArea = document.querySelector('textarea[name=responseBody]');
+const detailsArea = document.querySelector('textarea[name=responseDetails]');
 
 let maxi = false;
 
 sendBtn.addEventListener('click', () => {
-  const data = document.querySelector('textarea:first-child').value;
+  const data = document.querySelector('textarea[name=rawRequest]').value;
+  const post = document.querySelector('textarea[name=rawBody]').value;
   let req = {};
   req.method = data.split(' ')[0];
   req.url = data.split(' ')[1];
   req.headers = {};
+  if(post) {
+    req.reqData = post;
+  }
   data.split('\n').forEach((elm, i) => {
     if(i>0) {
       let header = {};
@@ -25,7 +31,10 @@ sendBtn.addEventListener('click', () => {
   }
   ipcRenderer.on('response', (events, args) => {
     responseArea.value = args;
-  })
+  });
+  ipcRenderer.on('details', (events, args) => {
+    detailsArea.value = JSON.stringify(args);
+  });
 });
 closeBtn.addEventListener('click', () => {
   ipcRenderer.send('close');
